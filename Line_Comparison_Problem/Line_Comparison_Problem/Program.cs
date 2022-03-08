34c1c6abd0b8cbed3444aa1b54bad6878b343e38
unknown <kinekarpallavi21@gmail.com>
2022-03-08 16:49:48 +0530

    Calculate Length

--- Line_Comparison_Problem/Line_Comparison_Problem/Program.cs
+++ Line_Comparison_Problem/Line_Comparison_Problem/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Line Comutation");
+﻿namespace Line_Comparison_Problem
+{
+
+    public class Example
+    {
+
+        static void Main(String[] args)
+        {
+
+            // point 1
+            double x1 = 10;
+            double y1 = 15;
+
+            // point 2
+            double x2 = 30;
+            double y2 = 5;
+
+            // Calculate distance between two points
+            double len = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            Console.WriteLine("Length between two points :" + len);
+        }
+    }
+}
